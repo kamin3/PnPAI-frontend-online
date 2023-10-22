b03514fbd9ad09cc256b421f809462b13de67926
WaleedElwakeel <waleed.elwakeel7@gmail.com>
2023-10-23 02:49:03 +0300

    fix auth layout

--- angular/src/app/modules/auth/pages/sign-in/sign-in.component.ts
+++ angular/src/app/modules/auth/pages/sign-in/sign-in.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CONFIG } from '@app/shared/configs';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+
+  homeRoute = CONFIG.home.route;
 
 }
