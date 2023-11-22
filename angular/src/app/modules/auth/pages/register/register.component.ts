f1d95a070d177dbddad8d9604c55feed3514a5a7
WaleedElwakeel <waleed.elwakeel7@gmail.com>
2023-11-23 00:18:59 +0200

    add sign in

--- angular/src/app/modules/auth/pages/register/register.component.ts
+++ angular/src/app/modules/auth/pages/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { City } from '@app/data/schema/city';
 import { Country } from '@app/data/schema/country';
 import { Industry } from '@app/data/schema/industry';
@@ -7,6 +7,7 @@ import { AccountService } from '@app/data/services/account.service';
 import { CountryService } from '@app/data/services/country.service';
 import { IndustryService } from '@app/data/services/industry.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CONFIG } from '@app/shared/configs';
 
 @Component({
   selector: 'app-register',
@@ -19,7 +20,12 @@ export class RegisterComponent implements OnInit {
   countries: Country[] = [];
   industries: Industry[] = [];
   cities: City[] = [];
-  reactiveForm: FormGroup | undefined;
+  registrationForm: FormGroup | undefined;
+  resultMessage: string | undefined = undefined;
+  resgiterationSuccess: boolean = false;
+  loginLink = CONFIG.auth.children.login.route;
+  @ViewChild('showresult') showModalBTN!: ElementRef<HTMLButtonElement>;
+
   constructor(
     private fb: FormBuilder,
     private countryService: CountryService,
@@ -29,7 +35,7 @@ export class RegisterComponent implements OnInit {
 
   }
   ngOnInit(): void {
-    this.reactiveForm = this.fb.group({
+    this.registrationForm = this.fb.group({
       companyName: ["", Validators.required],
       country: ["", Validators.required],
       city: ["", Validators.required],
@@ -39,7 +45,7 @@ export class RegisterComponent implements OnInit {
       industryId: ["", Validators.required],
       firstName: ["", [Validators.required]],
       lastName: ["", [Validators.required]],
-      phoneNumber: ["", Validators.required], // should validate against start with code + or 00
+      phoneNumber: [""], // should validate against start with code + or 00
       email: ["", [Validators.required, Validators.email]], // should validate against business email
       password: ["", [Validators.required]],
       acceptTerms: ["", Validators.requiredTrue],
@@ -72,46 +78,51 @@ export class RegisterComponent implements OnInit {
 
 
   onSubmit() {
-    if (!this.reactiveForm!.valid) {
+    if (!this.registrationForm!.valid) {
     }
     let input: userSignupInput = {
       "user_data": {
-        "user_id": this.reactiveForm?.value.email,
-        "first_name": this.reactiveForm?.value.firstName,
-        "last_name": this.reactiveForm?.value.lastName,
-        "email": this.reactiveForm?.value.email,
-        "phone": this.reactiveForm?.value.phoneNumber,
+        "user_id": this.registrationForm?.value.email,
+        "first_name": this.registrationForm?.value.firstName,
+        "last_name": this.registrationForm?.value.lastName,
+        "email": this.registrationForm?.value.email,
+        "phone": this.registrationForm?.value.phoneNumber,
         "role": "User"
       },
-      "raw_password": this.reactiveForm?.value.password,
+      "raw_password": this.registrationForm?.value.password,
       "org_data": {
-        "organization_name": this.reactiveForm?.value.companyName,
+        "organization_name": this.registrationForm?.value.companyName,
         "address": {
-          "postal_code": this.reactiveForm?.value.postalCode,
-          "street": this.reactiveForm?.value.street,
-          "city": this.reactiveForm?.value.city,
-          "country": this.reactiveForm?.value.country
+          "postal_code": this.registrationForm?.value.postalCode,
+          "street": this.registrationForm?.value.street,
+          "city": this.registrationForm?.value.city,
+          "country": this.registrationForm?.value.country
         },
         "status": "Active",
-        "industry_id": this.reactiveForm?.value.industryId,
-        "website_url": this.reactiveForm?.value.websiteurl
+        "industry_id": this.registrationForm?.value.industryId,
+        "website_url": this.registrationForm?.value.websiteurl
       }
     };
 
     this.accountService.userSignup(input).subscribe({
       next: (value) => {
-        console.log(value.message);
+        this.resultMessage = "Registeration successful";
+        this.resgiterationSuccess = true;
+        this.showModalBTN.nativeElement.click();
       },
       error: (err) => {
+        this.resultMessage = err;
+        this.resgiterationSuccess = false;
+        this.showModalBTN.nativeElement.click();
         console.log(err);
       },
     });
   }
 
   setAllTouched() {
-    Object.keys(this.reactiveForm!.controls).forEach(controlName => {
-      if (this.reactiveForm!.get(controlName)!.untouched)
-        this.reactiveForm!.get(controlName)!.markAsTouched();
+    Object.keys(this.registrationForm!.controls).forEach(controlName => {
+      if (this.registrationForm!.get(controlName)!.untouched)
+        this.registrationForm!.get(controlName)!.markAsTouched();
     });
   }
 
--- angular/src/app/modules/auth/pages/sign-in/sign-in.component.ts
+++ angular/src/app/modules/auth/pages/sign-in/sign-in.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { userSigninInput } from '@schema/userSigninInput';
+import { AccountService } from '@app/data/services/account.service';
 import { CONFIG } from '@app/shared/configs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
 
   homeRoute = CONFIG.home.route;
 
+  signinForm: FormGroup | undefined;
+
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private router: Router
+
+  ) {
+
+
+  }
+  ngOnInit(): void {
+    this.signinForm = this.fb.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+  }
+
+
+  onSubmit() {
+    if (!this.signinForm!.valid) {
+    }
+    let input: userSigninInput = {
+      "login_email": this.signinForm?.value.email,
+      "raw_password": this.signinForm?.value.password
+    };
+
+    this.accountService.signin(input).subscribe({
+      next: (value) => {
+        localStorage.setItem('accessToken', value.jwt_token);
+        this.router.navigateByUrl(this.homeRoute);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  };
+
+  setAllTouched() {
+    Object.keys(this.signinForm!.controls).forEach(controlName => {
+      if (this.signinForm!.get(controlName)!.untouched)
+        this.signinForm!.get(controlName)!.markAsTouched();
+    });
+  }
 }
