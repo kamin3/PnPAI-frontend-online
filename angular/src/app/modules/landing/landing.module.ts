e0b1f4f1e1c2d0a6f6d2c5bdae9e442f8042d8cf
WaleedElwakeel <waleed.elwakeel7@gmail.com>
2024-01-29 18:18:09 +0200

    creating home module

--- angular/src/app/modules/landing/landing.module.ts
+++ angular/src/app/modules/landing/landing.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '@app/shared/shared.module';
 import { LandingRoutingModule } from './landing-routing.module';
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 import { FaqComponent } from './pages/faq/faq.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
-  declarations: [LandingPageComponent, PrivacyPolicyComponent, FaqComponent],
+  declarations: [LandingPageComponent, PrivacyPolicyComponent, FaqComponent, HomePageComponent],
   imports: [RouterModule, LandingRoutingModule, SharedModule],
 })
 export class LandingModule { }
